803fa247576e950aeda0f7e9b6126f56380591f2
Justin KA <justinalexander101@gmail.com>
2023-08-10 22:18:08 -0400

    Added a navbar with shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Container } from "react-bootstrap"
 import { Store } from "./pages/Store"
 import { Home } from "./pages/Home"
 import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
-  <Container>
+  <>
+  <Navbar />
+  <Container className="mb-4">
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/store" element={<Store />}></Route>
       <Route path="/about" element={<About />}></Route>
     </Routes>
   </Container>
+  </>
   )
 }
 
