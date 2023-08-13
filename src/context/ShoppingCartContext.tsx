13167cf45fb9bf5e8e8e645f2ca0891af4ddb2a4
Justin KA <justinalexander101@gmail.com>
2023-08-13 16:04:14 -0400

    shopping cart uses localStorage

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useContext, useState } from "react";
 import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
@@ -31,7 +32,7 @@ export function useShoppingCart() {
 
 //radio that broadcasts the context
 export function ShoppingCartProvider({children} : ShoppingCartProviderProps) {
-    const [cartItems, setCartItems] = useState<CartItem[]> ([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]> ("shopping-cart",[])
     const [isOpen, setIsOpen] = useState(false)
 
     const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0)
