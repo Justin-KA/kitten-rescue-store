5fa9446bfa5371e5e0dc2d3c7af7f67deb0a56f5
Justin KA <justinalexander101@gmail.com>
2023-08-13 21:48:05 -0400

    added a basic homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,24 @@
+import { Card } from "react-bootstrap";
+
 export function Home(){
-    return <h1>Home</h1>
+    return (
+        <Card className="text-center h-200 text-light">
+            <Card.Img 
+            variant="top"
+            src = "../public/imgs/chestnoot.jpg"
+            height="500px"
+            style={{objectFit: "cover"}} />
+        <Card.ImgOverlay>
+          <Card.Body>
+            <Card.Title className="fs-1">The 'Adopt Don't Shop' Shop</Card.Title>
+            <Card.Text>
+              Buy a street cat, not a neat cat
+            </Card.Text>
+
+          </Card.Body>
+          </Card.ImgOverlay>
+          <Card.Footer className="text-dark">Look at this guy</Card.Footer>
+        </Card>
+      );
 }
+
